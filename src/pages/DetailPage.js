6099809e5cd28bf59d226829d83e3312b412e580
Santapol <sa.santapol_st@tni.ac.th>
2021-12-29 23:23:07 +0700

    Chapter 4 #2

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -11,13 +11,14 @@ import {
 } from "react-bootstrap";
 import axios from "axios";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 
 const DetailPage = () => {
   const { id, title } = useParams();
+  const history = useHistory();
   const [detail, setDetail] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
-
   const getData = async () => {
     try {
       setLoading(true); // เริ่มหมุนติ้วๆตรงนี้
@@ -33,19 +34,16 @@ const DetailPage = () => {
       setLoading(false); // หยุดตรงนี้ทุกกรณีที่ทำเสร็จว่าว่าจะ try หรือ catch ก็ตาม
     }
   };
-
   React.useEffect(() => {
     getData();
   }, []);
-
   if (loading === true) {
     return (
-        <div className="text-center mt-5">
+      <div className="text-center mt-5">
         <Spinner animation="border" variant="primary" />
       </div>
     );
   }
-
   if (error) {
     return (
       <div className="text-center mt-5 text-danger">
@@ -54,18 +52,20 @@ const DetailPage = () => {
       </div>
     );
   }
-
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12 mt-4">
           <h2>Detail Page</h2>
+          <Button variant="primary" onClick={() => {
+              history.goBack()
+          }}>Back</Button>
           <div className="row">
             <CardDeck>
               {detail.length > 0 ? (
                 detail.map((d, index) => {
                   return (
-                    <div className="col-md-4" key={d.ch_id}>
+                    <div className="col-md-4 mt-4" key={d.ch_id}>
                       <Card className="mb-4 shadow-sm">
                         {/* <Card.Img variant="top" src="" /> */}
                         <Card.Body>
@@ -87,9 +87,8 @@ const DetailPage = () => {
             </CardDeck>
           </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
-
 export default DetailPage;
