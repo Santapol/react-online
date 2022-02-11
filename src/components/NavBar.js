178081a04e4ba95d0dd3d900af4daa6bd80cec94
Santapol <sa.santapol_st@tni.ac.th>
2022-02-12 04:15:40 +0700

    1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
 import {NavLink, useHistory} from "react-router-dom"
+import {UserStoreContext} from '../context/UserContext'
+
 function NavBar() {
     const history = useHistory()
-    const [profile,setProfile] = React.useState(null)
-    const getProfile = () => {
-        //localStorage.getItem('profile')
-        const profileValue = JSON.parse(localStorage.getItem('profile'))
-        if(profileValue){
-            setProfile(profileValue)
-        }
+    const userStore = React.useContext(UserStoreContext)
+    //const [profile,setProfile] = React.useState(null)
+  // const getProfile = () => {
+    //     //localStorage.getItem('profile')
+    //     const profileValue = JSON.parse(localStorage.getItem('profile'))
+    //     if(profileValue){
+    //         setProfile(profileValue)
+    //     }
+    // }
+     // React.useEffect(() => {
+    //     getProfile()
+    // },[])
+    const logout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('profile')
+        history.replace('/')
+        history.go(0)
     }
-    React.useEffect(() => {
-         getProfile()
-     },[])
-
-     const logout = () => {
-         localStorage.removeItem('token')
-         localStorage.removeItem('profile')
-         history.replace('/')
-         history.go(0)
-     }
-
-     return (
-         <div>
-             <Navbar bg="light" expand="lg">
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
 
                     {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
                     <NavLink className="navbar-brand" exact to="/">React-Boostrap</NavLink>
@@ -48,27 +49,27 @@ function NavBar() {
                                 </NavDropdown>
                                 <NavLink className="nav-link" activeClassName="active" to="/upload">Upload</NavLink>
                                 <NavLink className="nav-link" activeClassName="active" to="/member">Member</NavLink>
-                                </Nav>
+                            </Nav>
+                            {
+                             userStore.profile ? (
+                                <span className='nav-text'>Welcome {userStore.profile.name} 
+                                    <button className='btn btn-danger ml-2'onClick={logout} >
+                                        Logout
+                                    </button>
+                                </span>
+                            ) : (
+                                <>
+                                    <Nav>
+                                        <NavLink className="nav-link" activeClassName="active" to="/register">Register</NavLink>
+                                        <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
+                                    </Nav>
+                                </>
+                            )
+                        }
+                    </Navbar.Collapse>
 
-{
-    profile ? (
-        <span className='nav-text'>Welcome {profile.name} 
-        <button className='btn btn-danger ml-2'onClick={logout} >
-            Logout
-        </button>
-    </span>
-) : (
-    <>
-        <Nav>
-           <NavLink className="nav-link" activeClassName="active" to="/register">Register</NavLink>
-           <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
-       </Nav>
-   </>
-)
-}
-</Navbar.Collapse>
-</Navbar>
-</div>
-)
+            </Navbar>
+        </div>
+    )
 }
 export default NavBar 
